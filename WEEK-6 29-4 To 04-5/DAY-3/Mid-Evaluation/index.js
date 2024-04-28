e15fab04c5969272b1720fd28414fb6ef069bf27
Nilesh Khairnar <nkhairnar543@gmail.com>
2024-04-28 18:09:56 +0530

    well

--- WEEK-6 29-4 To 04-5/DAY-3/Mid-Evaluation/index.js
+++ WEEK-6 29-4 To 04-5/DAY-3/Mid-Evaluation/index.js
@@ -1,19 +1,22 @@
 let tbody = document.querySelector("tbody");
+let next = document.getElementById("next")
 
+var count = 10;
+var page = 1;
 async function getData() {
   let res = await fetch(
-    "https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-employees"
+    `https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-employees?page=${page}&limit=${count}`
   );
   let data = await res.json();
   console.log(data);
   // append(data.data)
-  displayData(data);
+  displayData(data.data);
 }
 getData();
 
 function displayData(arr) {
   tbody.innerHTML = null;
-  arr.map(function (el,i) {
+  arr.forEach(function (el,i) {
     let tr = document.createElement("tr");
     let td0 = document.createElement("td");
     td0.innerText = el.id;
@@ -29,3 +32,12 @@ function displayData(arr) {
     tbody.append(tr);
   });
 }
+
+
+next.addEventListener("click",function(){
+  tbody.innerHTML = null;
+  count+=10;
+
+  getData();
+  
+})
